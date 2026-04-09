8676d03f9d5494420662ad19dd1ca61a48a1c28d
Sebastian Abe Santoso <sebastian.abe.santoso@gmail.com>
2026-04-09 17:13:22 +0800

    mengurangi variabel tidak dibutuhkan di prak102

--- MODUL1/PRAK102.php
+++ MODUL1/PRAK102.php
@@ -1,7 +1,6 @@
 <?php
     $Jari_jari = 4.2;
     $Tinggi = 5.4;
-    $Panjang = 8.9;
     $Volume_kerucut = 1/3 * 3.14 * $Jari_jari * $Jari_jari * $Tinggi;
     echo "Volume Kerucut : " . number_format($Volume_kerucut, 3) . " m3";
 ?>
