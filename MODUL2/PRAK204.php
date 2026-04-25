c094c7026f699e954544336bbcb4ed9693f9dbdd
Sebastian Abe Santoso <2410817210002@mhs.ulm.ac.id>
2026-04-26 02:00:48 +0800

    finished prak 3 and 4, laporan due tmrw

--- MODUL2/PRAK204.php
+++ MODUL2/PRAK204.php
@@ -1,26 +1,44 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
 <?php
-    $phones = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03", "Samsung Galaxy Xcover 5");
+$n = "";
+
+if (isset($_GET['number'])) {
+    $n = $_GET['number'];
+}
+
+?>
+<form method="get">
+    Nilai: <input type="text" name="number" value="<?php echo $n; ?>"> <br>
+    <input type="submit" value="Konversi"> <br>
+</form>
+
+<?php
+if (isset($_GET['number'])) {
+    if (!is_numeric($n)) { echo "<h2 style='color:red;'>Bukan angka</h2>";
+} else {
+    $n = (int) $n;
+    if ($n == 0) {
+        echo "<h2>Hasil: Nol</h2>";
+    } elseif ($n < 10) {
+        echo "<h2>Hasil: Satuan</h2>";
+    } elseif ($n < 20) {
+        echo "<h2>Hasil: Belasan</h2>";
+    } elseif ($n < 100) {
+        echo "<h2>Hasil: Puluhan</h2>";
+    } elseif ($n < 1000) {
+        echo "<h2>Hasil: Ratusan</h2>";
+    } else {
+        echo "<h2>Hasil: Melebihi limit</h2>";
+    }
+  }
+}
+
 ?>
 
-<table>
-    <tr><th>Daftar Smartphone Samsung</th></tr>
-    
-    <?php
-    foreach ($phones as $phone) {
-        echo "<tr>";
-        echo "<td>" . $phone . "</td>";
-        echo "</tr>";
-    }
-    ?>
-</table>    
+</body>
 
-<style>
-    table {
-        border-collapse: collapse;
-    }
-
-    td, th  {
-        border: 3px double black;
-    }
-
-</style>
+</html>
