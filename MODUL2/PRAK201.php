01b1d8ed7bf55365a9fa01278d21905b0ffbf06f
Sebastian Abe Santoso <sebastian.abe.santoso@gmail.com>
2026-04-26 02:06:09 +0800

    fixed indendatin

--- MODUL2/PRAK201.php
+++ MODUL2/PRAK201.php
@@ -11,53 +11,54 @@
 </form>
 
 <?php
-$name1 = $name2 = $name3 = "";
+    $name1 = $name2 = $name3 = "";
 
-if (isset($_GET['name1'], $_GET['name2'], $_GET['name3'])) {
+    if (isset($_GET['name1'], $_GET['name2'], $_GET['name3'])) {
 
-    $name1 = $_GET['name1'];
-    $name2 = $_GET['name2'];
-    $name3 = $_GET['name3'];
+        $name1 = $_GET['name1'];
+        $name2 = $_GET['name2'];
+        $name3 = $_GET['name3'];
 
-    if ($name1 <= $name2 && $name1 <= $name3) {
-        $first = $name1;
+        if ($name1 <= $name2 && $name1 <= $name3) {
+            $first = $name1;
 
-        if ($name2 <= $name3) {
-            $second = $name2;
-            $third = $name3;
+            if ($name2 <= $name3) {
+                $second = $name2;
+                $third = $name3;
+            } else {
+                $second = $name3;
+                $third = $name2;
+            }
+
+        } elseif ($name2 <= $name1 && $name2 <= $name3) {
+            $first = $name2;
+
+            if ($name1 <= $name3) {
+                $second = $name1;
+                $third = $name3;
+            } else {
+                $second = $name3;
+                $third = $name1;
+            }
+
         } else {
-            $second = $name3;
-            $third = $name2;
+            $first = $name3;
+
+            if ($name1 <= $name2) {
+                $second = $name1;
+                $third = $name2;
+            } else {
+                $second = $name2;
+                $third = $name1;
+            }
         }
 
-    } elseif ($name2 <= $name1 && $name2 <= $name3) {
-        $first = $name2;
-
-        if ($name1 <= $name3) {
-            $second = $name1;
-            $third = $name3;
-        } else {
-            $second = $name3;
-            $third = $name1;
-        }
-
-    } else {
-        $first = $name3;
-
-        if ($name1 <= $name2) {
-            $second = $name1;
-            $third = $name2;
-        } else {
-            $second = $name2;
-            $third = $name1;
-        }
+        echo "$first <br>";
+        echo "$second <br>";
+        echo "$third <br>";
     }
-
-    echo "$first <br>";
-    echo "$second <br>";
-    echo "$third <br>";
-}
 ?>
 
 </body>
+        
 </html>
