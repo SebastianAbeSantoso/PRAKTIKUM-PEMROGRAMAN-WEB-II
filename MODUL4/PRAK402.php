5d34c5d26461a9a1f6bace2b6ae0e92d50ebf024
Sebastian Abe Santoso <2410817210002@mhs.ulm.ac.id>
2026-05-22 00:50:17 +0800

    402 403 done

--- MODUL4/PRAK402.php
+++ MODUL4/PRAK402.php
@@ -1,42 +1,90 @@
 <!DOCTYPE html>
 
 <html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 35%;
+        }
 
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #d0cece;
+        }
+    </style>
+</head>
 <body>
     <table>
         <tr>
-            <th>Name</th>
+            <th>Nama</th>
             <th>NIM</th>
-            <th>UTS Score</th>
-            <th>UAS Score</th>
+            <th>Nilai UTS</th>
+            <th>Nilai UAS</th>
+            <th>Nilai Akhir</th>
+            <th>Huruf</th>
         </tr>
 
+
+
 <?php
-    $student = array();
-    $student['Andi'] = array(
-    "NIM" => "2101001", 
-    "UTS_SCORE" => "87", 
-    "UAS_SCORE" => "65",
-);
+    function getLetterGrade($score) {
+        if ($score >= 80) {
+            return 'A';
+        } elseif ($score >= 70) {
+            return 'B';
+        } elseif ($score >= 60) {
+            return 'C';
+        } elseif ($score >= 50) {
+            return 'D';
+        } else {
+            return 'E';
+        }
+    }
+
+    $student = array(
+        'Andi' => array(
+            "NIM" => "2101001", 
+            "UTS_SCORE" => "87", 
+            "UAS_SCORE" => "65",
+        ),
+        'Budi' => array(
+            "NIM" => "2101002", 
+            "UTS_SCORE" => "76", 
+            "UAS_SCORE" => "79",
+        ),
+        'Tono' => array(
+            "NIM" => "2101003", 
+            "UTS_SCORE" => "50", 
+            "UAS_SCORE" => "41",
+        ),
+        'Jessica' => array(
+            "NIM" => "2101004", 
+            "UTS_SCORE" => "60", 
+            "UAS_SCORE" => "75",
+        ),
+    );
 
     foreach ($student as $name => $details) {
-      echo "<tr>";
+        $final_score = 0.4 * $details['UTS_SCORE'] + 0.6 * $details['UAS_SCORE'];
 
-      foreach ($details as $key => $value) {
-        if (empty($value)) {
-          $details[$key] = "null";
-        } else {
-            echo "<td>" . $name . "</td>";
-            echo "<td>" . $details['NIM'] . "</td>";
-            echo "<td>" . $details['UTS_SCORE'] . "</td>";
-            echo "<td>" . $details['UAS_SCORE'] . "</td>";
-        }
-      }
-      echo "</tr>";
+        echo "<tr>";
+        echo "<td>" . $name . "</td>";
+        echo "<td>" . $details['NIM'] . "</td>";
+        echo "<td>" . $details['UTS_SCORE'] . "</td>";
+        echo "<td>" . $details['UAS_SCORE'] . "</td>";
+        echo "<td>" . $final_score . "</td>";
+        echo "<td>" . getLetterGrade($final_score) . "</td>";
+        echo "</tr>";
     }
 ?>
 </table>
 
 </body>
-  
+
 </html>
--- MODUL4/PRAK403.php
+++ MODUL4/PRAK403.php
@@ -1,31 +1,112 @@
 <!DOCTYPE html>
 
 <html>
+<head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 30%;
+        }
 
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: left;
+        }
+
+        th {
+            background-color: #d0cece;
+        }
+
+    </style>
+</head>
 <body>
+    <table>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Mata Kuliah diambil</th>
+            <th>SKS</th>
+            <th>Total SKS</th>
+            <th>Keterangan</th>
+        </tr>
 
-<form method="post">
-    Batas Bawah : <input type="text" name="bottom" value="<?php echo isset($_POST['bottom']) ? $_POST['bottom'] : ''; ?>"> <br>
-    Batas Atas : <input type="text" name="top" value="<?php echo isset($_POST['top']) ? $_POST['top'] : ''; ?>"> <br>
-    <input type="submit" name="submit" value="Cetak"> <br> <br>
-</form>
+
 
 <?php
-  if (isset($_POST['submit']) && isset($_POST['bottom']) && isset($_POST['top'])) {
-    $bottom = $_POST['bottom'];
-    $top = $_POST['top'];
-    $image = "https://www.freepnglogos.com/uploads/star-png/file-featured-article-star-svg-wikimedia-commons-8.png";
-do {
-    if (($bottom + 7) % 5 === 0) {
-        echo "<img src='$image' width='20' height='20'> ";
+$student = array(
+    'Ridho' => array(
+        "Course" => array(
+            "Pemrograman I" => 2,
+            "Praktikum Pemrograman I" => 1,
+            "Pengantar Lingkungan Lahan Basah" => 2,
+            "Arsitektur Komputer" => 3,
+        ), 
+    ),
+    'Ratna' => array(
+        "Course" => array(
+            "Basis Data I" => 2,
+            "Praktikum Basis Data I" => 1,
+            "Kalkulus" => 3,
+        ),
+    ),
+    'Tono' => array(
+        "Course" => array(
+            "Rekayasa Perangkat Lunak" => 3,
+            "Analisis dan Perancangan Sistem" => 3,
+            "Komputasi Awan" => 3,
+            "Kecerdasan Bisnis" => 3,
+        ), 
+    ),
+);
+
+$no = 1;
+
+foreach ($student as $name => $details) {
+    $courses = $details["Course"];
+    $total_sks = array_sum($courses);
+
+    if ($total_sks >= 7) {
+        $keterangan = "Tidak Revisi";
+        $color = "green";
     } else {
-        echo "$bottom ";
+        $keterangan = "Revisi KRS";
+        $color = "red";
     }
-    $bottom++;
-} while ($bottom <= $top);
+
+    $firstRow = true;
+
+    foreach ($courses as $course => $sks) {
+        echo "<tr>";
+
+        if ($firstRow) {
+            echo "<td>" . $no . "</td>";
+            echo "<td>" . $name . "</td>";
+        } else {
+            echo "<td></td>";
+            echo "<td></td>";
+        }
+
+        echo "<td>" . $course . "</td>";
+        echo "<td>" . $sks . "</td>";
+
+        if ($firstRow) {
+            echo "<td>" . $total_sks . "</td>";
+            echo "<td style='background-color: $color;'>" . $keterangan . "</td>";
+        } else {
+            echo "<td></td>";
+            echo "<td></td>";
+        }
+
+        echo "</tr>";
+
+        $firstRow = false;
+    }
+
+    $no++;
 }
 ?>
-
+</table>
 
 </body>
 
